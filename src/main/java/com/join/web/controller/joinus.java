e5e20c9cf8253bdd9dd280f20135c34cc702b2ad
VanilaJelly <sumin.lee816@gmail.com>
2017-03-02 18:09:20 +0900

    edited joinus.java
    add some download codes

--- src/main/java/com/join/web/controller/joinus.java
+++ src/main/java/com/join/web/controller/joinus.java
@@ -2,6 +2,9 @@ package com.join.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.ui.Model;
 import com.join.web.model.RequestModel;
 
@@ -30,4 +33,13 @@ public class joinus {
 		
 		return "welcome";
 	}
+	
+	 @RequestMapping(method=RequestMethod.POST)
+	 public String handleFile(MultipartHttpServletRequest request)
+	 {
+	  MultipartFile file = request.getFile("filedata");
+	  //some code here
+	  return "fileupload/success";
+	 }
+
 }
